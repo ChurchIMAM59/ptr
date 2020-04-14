787eaad1b400dfc4d306e121ba01ba46786f4c4b
ChurchIMAM59 <63426586+ChurchIMAM59@users.noreply.github.com>
2020-04-14 13:39:58 +0300

    Update degiskenler.php

--- batuhanhuner/degiskenler.php
+++ batuhanhuner/degiskenler.php
@@ -26,17 +26,10 @@ $user_bio ="
 
 
 
-	Every avid independent filmmaker has <b>Bold</b> about making that <i>Italic</i> interest documentary, or short
-  film to show off their creative prowess. Many have great ideas and want to “wow” the<sup>Superscript</sup> scene,
-  or video renters with their big project. But once you have the<sub>Subscript</sub> “in the can” (no easy feat), how
-  do you move from a <del>Strike</del> through of master DVDs with the <u>“Underline”</u> marked hand-written title
-  inside a secondhand CD case, to a pile of cardboard boxes full of shiny new, retail-ready DVDs, with UPC barcodes
-  and polywrap sitting on your doorstep? You need to create eye-popping artwork and have your project replicated.
-  Using a reputable full service DVD Replication company like PacificDisc, Inc. to partner with is certainly a
-  helpful option to ensure a professional end result, but to help with your DVD replication project, here are 4 easy
-  steps to follow for good DVD replication results: falan filan fistak
-
-
+	Herkese merhaba ismim Batuhan 19 yaşındayım İstanbul Rumeli Üniversitesin de Bilgisayar Programcılığı 1.sınıf öğrencisiyim.
+          Tekirdağ da yaşıyorum günlük yaşantımda sürekli bilgisayarla iç içeyim. Sürekli kendimi geliştirmekle uğraşıyorum.
+          Çılgın ve eğlenceli bir kişiliğim. Boş zamanlarımda araştırmalar yaparak kendime bir şeyler katmaya çalışıyorum.
+          
   ";
 $user_adsoyad="Batuhan Hüner";
 $user_mail ="batuhan_huner59@hotmail.com";
